e13a13898d967ae3b6680425aa77dab82ed86e5a
Phu Quõc <you@example.com>
2024-08-19 21:04:17 +0700

    add necessary library, some models ~~

---
+++ phone-commerce/src/main/java/com/example/phonecommerce/models/Role.java
@@ -0,0 +1,30 @@
+package com.example.phonecommerce.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+    @Override
+    public String toString() {
+        return "Role [id=" + id + ", name=" + name + "]";
+    }
+}
---
+++ phone-commerce/src/main/java/com/example/phonecommerce/models/User.java
@@ -0,0 +1,52 @@
+package com.example.phonecommerce.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String address;
+
+    @Column(nullable = false, unique = true)
+    @NotEmpty
+    private String email;
+    private String userName;
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    @NotEmpty
+    private String password;
+
+
+    @ManyToMany(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role",
+            joinColumns = {@JoinColumn(name = "USER_ID", referencedColumnName = "ID")},
+            inverseJoinColumns = {@JoinColumn(name = "ROLE_ID", referencedColumnName = "ID")}
+    )
+    private List<Role> roles;
+
+//    @OneToMany(mappedBy = "userOrder", cascade = CascadeType.ALL)
+//    private List<Order> orders;
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", name=" + name + ", address=" + address + ", email=" + email + "]";
+    }
+
+}
