157c0d4bb435837522dcf5f4f9afc1cc896f1e67
Phú Quốc <dinhphuquoc2003@gmail.com>
2024-08-31 09:23:55 +0700

    add security part 3 ~~

--- phone-commerce/src/main/java/com/example/phonecommerce/security/SecurityConfig.java
+++ phone-commerce/src/main/java/com/example/phonecommerce/security/SecurityConfig.java
@@ -1,10 +1,49 @@
 package com.example.phonecommerce.security;
 
 
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+@Configurable
+@EnableWebSecurity
 public class SecurityConfig {
+
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    };
+
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+//        authenticationProvider.setUserDetailsService();
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.logout(LogoutConfigurer::permitAll);
+
+
+
+
+        return http.build();
+    }
+
+
+
+
 
 
 
