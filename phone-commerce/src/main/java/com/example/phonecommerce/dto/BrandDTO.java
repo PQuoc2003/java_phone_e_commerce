344f17337b1b0b61336d17291566eef5ab797cd8
Phú Quốc <dinhphuquoc2003@gmail.com>
2024-09-30 20:17:36 +0700

    add brandDTO and modify method ~~

--- phone-commerce/src/main/java/com/example/phonecommerce/dto/BrandDTO.java
+++ phone-commerce/src/main/java/com/example/phonecommerce/dto/BrandDTO.java
@@ -1,8 +1,13 @@
 package com.example.phonecommerce.dto;
 
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class BrandDTO {
-
-
-
-
+    private Long id;
+    private String name;
 }
