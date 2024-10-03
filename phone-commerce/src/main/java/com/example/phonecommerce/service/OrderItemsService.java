85867d53dbc63fcf0707312e14d768e5252c7e9e
Phú Quốc <dinhphuquoc2003@gmail.com>
2024-10-03 21:55:50 +0700

    add orderDTO ~~

--- phone-commerce/src/main/java/com/example/phonecommerce/service/OrderItemsService.java
+++ phone-commerce/src/main/java/com/example/phonecommerce/service/OrderItemsService.java
@@ -4,7 +4,6 @@ import com.example.phonecommerce.models.OrderItems;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public interface OrderItemsService {
