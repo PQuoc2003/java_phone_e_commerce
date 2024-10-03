a398537dd27543ca17a81334efaf065c8313c37c
Phú Quốc <dinhphuquoc2003@gmail.com>
2024-10-03 10:00:13 +0700

    add product details page ~~

--- phone-commerce/src/main/java/com/example/phonecommerce/dto/ProductDTO.java
+++ phone-commerce/src/main/java/com/example/phonecommerce/dto/ProductDTO.java
@@ -1,5 +1,6 @@
 package com.example.phonecommerce.dto;
 
+import com.example.phonecommerce.models.Product;
 import lombok.*;
 
 @Data
@@ -24,6 +25,20 @@ public class ProductDTO {
     private String color;
 
     private String category;
+
+
+    public void convertToDTO(Product product){
+
+        this.id = product.getId();
+        this.name = product.getName();
+        this.brand = product.getBrand() != null ? product.getBrand().getName() : "N/A";
+        this.color = product.getColor() != null ? product.getColor().getName() : "N/A";
+        this.description = product.getDescription() != null ? product.getDescription() : "N/A";
+        this.category = product.getCategory() != null ? product.getCategory().getName(): "N/A";
+        this.picture = product.getPicture();
+        this.price = product.getPrice();
+    }
+
 
 
 }
