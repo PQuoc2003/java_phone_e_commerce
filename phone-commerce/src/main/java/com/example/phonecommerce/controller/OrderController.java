f073e797496573e4dc9540745028ae5f63f4d59d
Phú Quốc <dinhphuquoc2003@gmail.com>
2024-10-04 22:16:13 +0700

    vnpay config 1 ~~

--- phone-commerce/src/main/java/com/example/phonecommerce/controller/OrderController.java
+++ phone-commerce/src/main/java/com/example/phonecommerce/controller/OrderController.java
@@ -31,15 +31,14 @@ public class OrderController {
     }
 
 
-
     @GetMapping("admin/orders")
-    public String getAdminOrderPage(Model model){
+    public String getAdminOrderPage(Model model) {
 
         List<Orders> orders = ordersService.getAllOrder();
 
-        List<OrdersDTO> ordersDTOS = new ArrayList<OrdersDTO>();
+        List<OrdersDTO> ordersDTOS = new ArrayList<>();
 
-        for (Orders order: orders) {
+        for (Orders order : orders) {
             OrdersDTO ordersDTO = new OrdersDTO();
             ordersDTO.convertToDTO(order);
             ordersDTOS.add(ordersDTO);
@@ -53,19 +52,19 @@ public class OrderController {
     }
 
     @GetMapping("/admin/orders/delete/{id}")
-    public String deleteOrder(@PathVariable Long id){
+    public String deleteOrder(@PathVariable Long id) {
         ordersService.deleteById(id);
         return "redirect:/admin/orders";
     }
 
     @GetMapping("/admin/orders/edit/{id}")
-    public String updateStatus(@PathVariable Long id){
+    public String updateStatus(@PathVariable Long id) {
         ordersService.updateStatus(id);
         return "redirect:/admin/orders";
     }
 
     @GetMapping(value = "/orders-page")
-    public String orderPage(@RequestParam("vnp_ResponseCode") String responseCode, Model model, HttpSession session){
+    public String orderPage(@RequestParam("vnp_ResponseCode") String responseCode, Model model, HttpSession session) {
         if (responseCode.equals("00")) {
             // Lấy orderDetails từ session
             OrdersDTO ordersDTO = (OrdersDTO) session.getAttribute("orderDetails");
@@ -83,19 +82,12 @@ public class OrderController {
             orders.setOrder_total(ordersDTO.getTotal());
             orders.setOrder_status("Chưa Giao Hàng");
 
-            return "orders-details";
-        }else {
-            return "orders-details";
+
         }
 
+        return "orders-details";
+
     }
-
-
-
-
-
-
-
 
 
 }
